b0fb268ef789de0794b298ad1d2d04239dc6ddec
deboradeoliveira <70296404+deboradeoliveira@users.noreply.github.com>
2021-11-20 18:19:34 -0300

    Implementado envio de mensagem

--- GirlsLoveTech/app/src/main/java/com/example/girlslovetech/Mensagem.java
+++ GirlsLoveTech/app/src/main/java/com/example/girlslovetech/Mensagem.java
@@ -2,7 +2,10 @@ package com.example.girlslovetech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Mensagem extends AppCompatActivity {
 
@@ -10,7 +13,16 @@ public class Mensagem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mensagem);
+
     }
 
+
+    public void btnEnviarClick(View view){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT,"Olha que legal o que eu encontrei! Um site com aulas gratuitas sobre programação! https://womancancode.herokuapp.com/");
+        intent.setType("text/plain");
+        startActivity(intent);
+    }
 
 }
